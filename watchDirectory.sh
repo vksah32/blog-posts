025e126d3e4de9295b45f94bd8aa150e89661553
Vivek Sah <vivek.sah32+github@gmail.com>
2020-05-17 20:49:43 -0700

    Added watchDirectory.sh

---
+++ watchDirectory.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+fswatch -e ".*" -i "\\.md$" -x --event Created --event Updated -0 ./ | while read -d "" event 
+do 
+    # check any untracked files
+    for f in `git ls-files --others --exclude-standard`; do
+		git add "$f"
+		git commit -m "Added $f"
+    done
+
+    #check any modified files
+    for f in `git ls-files --modified`; do
+    	git add "$f"
+    	git commit -m "Modified $f"
+    done
+    git push origin master
+done
